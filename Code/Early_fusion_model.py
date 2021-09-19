0e515b0a4a81ccf52219b1df20715230e7a8f96d
ibrahim-radwan <radwanebrahim@gmail.com>
2021-09-19 10:55:19 +1000

    Update Early_fusion_model.py

--- Code/Early_fusion_model.py
+++ Code/Early_fusion_model.py
@@ -13,7 +13,7 @@ from keras.layers import Dense, Input, Dropout
 def build_model(inp_sizes, out_size):
 
     model1_in = Input(shape=(inp_sizes,))
-    l1 = Dense(8182, activation='relu', kernel_initializer="he_normal", name='merged_layer_1')(model1_in)
+    l1 = Dense(8192, activation='relu', kernel_initializer="he_normal", name='merged_layer_1')(model1_in)
     l2 = Dropout(0.5)(l1)
     l3 = Dense(4096, activation='relu', kernel_initializer="he_normal", name='merged_layerdsfssad2')(l2)
     l4 = Dropout(0.5)(l3)
